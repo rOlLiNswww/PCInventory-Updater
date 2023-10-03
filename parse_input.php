b1e8077c4f6082585293ff699635574a96eb52b3
Rollins <rollinswww0109@gmail.com>
2023-10-04 10:52:13 +1100

    add loop for muti inventory input

--- parse_input.php
+++ parse_input.php
@@ -1,32 +1,25 @@
 <?php
 
-function parseInputData() {
-    $data = file_get_contents('input.json');
-    $jsonDataArray = json_decode($data, true);
-    $result = [];
-
-    if (is_array($jsonDataArray)) {
-        foreach ($jsonDataArray as $jsonData) {
-            $result[] = [
-                "code" => $jsonData['HEADER CODE'],
-                "stockOnhand" => $jsonData['STOCKONHAND'],
-                "incoming" => $jsonData['INCOMING'],
-                "qty1" => (string) $jsonData['QTY1'],
-                "price1" => $jsonData['BLANK PRICE 1'],
-                "qty2" => (string) $jsonData['QTY2'],
-                "price2" => $jsonData['BLANK PRICE 2'],
-                "qty3" => (string) $jsonData['QTY3'],
-                "price3" => $jsonData['BLANK PRICE 3'],
-                "qty4" => (string) $jsonData['QTY4'],
-                "price4" => $jsonData['BLANK PRICE 4'],
-                "qty5" => (string) $jsonData['QTY5'],
-                "price5" => $jsonData['BLANK PRICE 5'],
-                "qty6" => (string) $jsonData['QTY6'],
-                "price6" => $jsonData['BLANK PRICE 6']      
-            ];
-        }
-    }
-
+function parseInputData($jsonDataArray) {
+   
+    $result = [ 
+        "code" => $jsonDataArray['HEADER CODE'],
+        "stockOnhand" => $jsonDataArray['STOCKONHAND'],
+        "incoming" => $jsonDataArray['INCOMING'],
+        "qty1" => (string) $jsonDataArray['QTY1'],
+        "price1" => $jsonDataArray['BLANK PRICE 1'],
+        "qty2" => (string) $jsonDataArray['QTY2'],
+        "price2" => $jsonDataArray['BLANK PRICE 2'],
+        "qty3" => (string) $jsonDataArray['QTY3'],
+        "price3" => $jsonDataArray['BLANK PRICE 3'],
+        "qty4" => (string) $jsonDataArray['QTY4'],
+        "price4" => $jsonDataArray['BLANK PRICE 4'],
+        "qty5" => (string) $jsonDataArray['QTY5'],
+        "price5" => $jsonDataArray['BLANK PRICE 5'],
+        "qty6" => (string) $jsonDataArray['QTY6'],
+        "price6" => $jsonDataArray['BLANK PRICE 6']      
+    ];
+        
     return $result; 
 }
 
--- start.php
+++ start.php
@@ -1,14 +1,15 @@
 <?php
 
-require_once 'parse_input.php'; // 引入第一个文件
-require_once 'execute_mutation.php'; // 引入第二个文件
+require_once 'parse_input.php';
+require_once 'execute_mutation.php';
 
-// 调用第一个文件中的函数，解析数据
-$dataArray = parseInputData();
+$data = file_get_contents('input.json');
+$jsonDataArray = json_decode($data, true);
 
-// 使用解析得到的数据调用第二个文件中的函数，执行每一个mutation
-foreach ($dataArray as $data) {
-    executeMutation(["input" => $data]);
+if (is_array($jsonDataArray)) {
+    foreach ($jsonDataArray as $jsonData) {
+        $dataArray = parseInputData($jsonData);
+        executeMutation(["input" => $dataArray]);
+    }
 }
-
 ?>
